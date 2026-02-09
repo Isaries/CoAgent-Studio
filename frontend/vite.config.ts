403c1beb72574d572e71d461dee3751ab80cee18
Isaries <leolove3very@gmail.com>
2026-02-09 20:12:13 +0800

    Fix(frontend): use sass modern api instead of modern-compiler to support js implementation

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern',
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
